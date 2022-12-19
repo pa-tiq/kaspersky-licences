61fde831ad13351b4ab21e31af82450d20aca70f
pa-tiq <patrickdsilva99@gmail.com>
2022-12-19 11:02:51 -0400

    porcentagem

--- src/components/Licences/LicenceList.js
+++ src/components/Licences/LicenceList.js
@@ -14,7 +14,7 @@ const LicenceList = (props) => {
         qtdTotal += item['qtd licencas'];
         return (<LicenceItem
           key={item.OM}
-          title={item.OM}
+          title={item.OM === 'Managed devices' ? 'Servidor' : item.OM}
           qtd={item['qtd licencas']}
         />);
       });
@@ -26,6 +26,11 @@ const LicenceList = (props) => {
         key={'Qtd Total'}
         title={'Qtd Total'}
         qtd={qtdTotal}
+      />      
+      <LicenceItem 
+        key={'Porcentagem'}
+        title={'Porcentagem do total de 3000'}
+        qtd={(qtdTotal / 3000) * 100}
       />
     </ul>
   )
